67a18eba058633ac061b61cab537e8c79a7786a9
pgk95 <kalupgk@gmail.com>
2023-04-26 02:39:18 -0700

    quick fix to task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
-#include <sort.h>
+#include "sort.h"
 
 /**
  * bubble_sort - Arranges numbers or other elements in the correct order.
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * @array: The array to be sorted.
+ * @size: The size of the array.
  */
-
 
 void bubble_sort(int *array, size_t size)
 {
-	int temp, i, j;
+	size_t i, j;
+	int temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (arr[j] > arr[j + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
 	}
--- sort.h
+++ sort.h
@@ -1,6 +1,18 @@
 #ifndef SORT_H
 #define SORT_H
 
+/*Defining listint_t type */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+
+/*These are the Function prototypes*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
