013d4ee61dd6b242cd581932df1c58483bcc3b4f
pgk95 <kalupgk@gmail.com>
2023-04-26 04:35:24 -0700

    adv task 4

--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void shell_sort(int *array, size_t size);
+
 
 #endif
